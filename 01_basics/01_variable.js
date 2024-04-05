7cbc5b71f2ab5b98241e3a9eed46db4cec183640
Devendra190404 <72194247+Devendra190404@users.noreply.github.com>
2024-04-05 20:56:52 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountid =144553
+let accountemail = "devendrasahu@gmail.com"
+var accountpassword="121312"
+accountcity="jaipur"
+let accountstate
+// accountid=55;
+
+accountemail="devendrakumar@gmail.com"
+accountpassword=" 12313378346"
+// console.log(accountid);
+// console.log(accountemail);
+// console.log(accountpassword);
+console.table([accountid,accountstate,accountemail,accountcity,accountpassword])
